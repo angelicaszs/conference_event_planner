16d30b3ed278f551b599ae34991e97374d727d6e
angelicaszs <angelica.suarez@awaze.com>
2025-07-15 14:58:14 +0200

    initial commit - base code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import "./App.css";
-import ConferenceEvent from "./ConferenceEvent";
-import AboutUs from "./AboutUs";
+import { useState } from 'react';
+import './App.css';
+import ConferenceEvent from './ConferenceEvent';
+import AboutUs from './AboutUs';
 
 function App() {
   const [showVenue, setShowVenue] = useState(false);
@@ -12,18 +12,24 @@ function App() {
 
   return (
     <>
-      <header className="first_page">
-        <div className="main_event">
-          <div className="first_page_name_btn">
-            <h1 className="budget_heading">Conference Expense Planner</h1>
-            <p className="budget_sentence"> Plan your next major event with us!</p>
-            <div className="getstarted_btn">
-              <button onClick={() => handleGetStarted()} className="get-started-btn">
+      <header className='first_page'>
+        <div className='main_event'>
+          <div className='first_page_name_btn'>
+            <h1 className='budget_heading'>Conference Expense Planner</h1>
+            <p className='budget_sentence'>
+              {' '}
+              Plan your next major event with us!
+            </p>
+            <div className='getstarted_btn'>
+              <button
+                onClick={() => handleGetStarted()}
+                className='get-started-btn'
+              >
                 Get Started
               </button>
             </div>
           </div>
-          <div className="aboutus_main">
+          <div className='aboutus_main'>
             <AboutUs />
           </div>
         </div>
