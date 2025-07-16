88210c6f2cc248201ceb409cdf513642641d41b3
angelicaszs <angelica.suarez@awaze.com>
2025-07-16 11:33:43 +0200

    add final table and price

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './ConferenceEvent.css';
 import TotalCost from './TotalCost';
 import { useSelector, useDispatch } from 'react-redux';
@@ -51,24 +52,82 @@ const ConferenceEvent = () => {
     dispatch(toggleMealSelection(index));
   };
 
-  // const handleMealSelection = (index) => {
-  //   const item = mealsItems[index];
-  //   if (item.selected && item.type === 'mealForPeople') {
-  //     // Ensure numberOfPeople is set before toggling selection
-  //     const newNumberOfPeople = item.selected ? numberOfPeople : 0;
-  //     dispatch(toggleMealSelection(index, newNumberOfPeople));
-  //   } else {
-  //     dispatch(toggleMealSelection(index));
-  //   }
-  // };
-
   const getItemsFromTotalCost = () => {
     const items = [];
+    venueItems.forEach((item) => {
+      if (item.quantity > 0) {
+        items.push({ ...item, type: 'venue' });
+      }
+    });
+    avItems.forEach((item) => {
+      if (
+        item.quantity > 0 &&
+        !items.some((i) => i.name === item.name && i.type === 'av')
+      ) {
+        items.push({ ...item, type: 'av' });
+      }
+    });
+    mealsItems.forEach((item) => {
+      if (item.selected) {
+        const itemForDisplay = { ...item, type: 'meals' };
+        items.push(itemForDisplay);
+      }
+    });
+    return items;
   };
 
   const items = getItemsFromTotalCost();
 
-  const ItemsDisplay = ({ items }) => {};
+  const ItemsDisplay = ({ items }) => {
+    console.log(items);
+    return (
+      <>
+        <div className='display_box1'>
+          {items.length === 0 && <p>No items selected</p>}
+          <table className='table_item_data'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Unit Cost</th>
+                <th>Quantity</th>
+                <th>Subtotal</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>${item.cost}</td>
+                  <td>
+                    {item.type === 'meals'
+                      ? ` For ${numberOfPeople} people`
+                      : item.quantity}
+                  </td>
+                  <td>
+                    {item.type === 'meals'
+                      ? `${item.cost * numberOfPeople}`
+                      : `${item.cost * item.quantity}`}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  };
+
+  ItemsDisplay.propTypes = {
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        cost: PropTypes.number.isRequired,
+        quantity: PropTypes.number,
+        selected: PropTypes.bool,
+        type: PropTypes.string
+      })
+    ).isRequired
+  };
 
   const calculateTotalCost = (section) => {
     let totalCost = 0;
--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import './TotalCost.css';
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
+  const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
   return (
     <div className='pricing-app'>
       <div className='display_box'>
@@ -11,13 +12,33 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
           </p>
         </div>
         <div>
-          <h2 id='pre_fee_cost_display' className='price'></h2>
-
-          <div></div>
+          <h2 id='pre_fee_cost_display' className='price'>
+            {total_amount}
+          </h2>
+          <div className='render_items'>
+            <ItemsDisplay />
+          </div>
         </div>
       </div>
     </div>
   );
+};
+
+TotalCost.propTypes = {
+  totalCosts: PropTypes.shape({
+    venue: PropTypes.number.isRequired,
+    av: PropTypes.number.isRequired,
+    meals: PropTypes.number.isRequired
+  }).isRequired,
+  ItemsDisplay: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      cost: PropTypes.number.isRequired,
+      quantity: PropTypes.number,
+      selected: PropTypes.bool,
+      type: PropTypes.string
+    })
+  ).isRequired
 };
 
 export default TotalCost;
